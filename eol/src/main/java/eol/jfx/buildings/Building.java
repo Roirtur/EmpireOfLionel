c8db9e773b691c35980b6078cd9fcc34f58deb0b
Gabriel Ifrim <gabriel.ifrim@etu.u-bordeaux.fr>
2024-11-18 18:53:31 +0100

    GameManager added, and implementation of finding house for inhabitants

--- eol/src/main/java/eol/jfx/buildings/Building.java
+++ eol/src/main/java/eol/jfx/buildings/Building.java
@@ -1,142 +1,178 @@
 package eol.jfx.buildings;
 
-import java.util.HashMap;
-
 import eol.jfx.residents.works.Work;
 import eol.jfx.ressources.PlayerInventory;
 import eol.jfx.ressources.Ressource;
+import java.util.HashMap;
 
 public abstract class Building {
 
-    public final int x, y;
-    private final int width, height;
+  public final int x, y;
+  private final int width, height;
 
-    private final int maxResidents;
-    public int currentResidents;
+  private final int maxResidents;
+  public int currentResidents;
 
-    private final int maxWorkers;
-    public int currentWorkers;
+  private final int maxWorkers;
+  public int currentWorkers;
 
-    // Hashmap of ressources for construction cost
-    private final HashMap<Ressource, Integer> constructionCost;
-    private final int constructionTime;
+  // Hashmap of ressources for construction cost
+  private final HashMap<Ressource, Integer> constructionCost;
+  private final int constructionTime;
 
-    public boolean isBuilt = false;
+  public boolean isBuilt = false;
 
-    private Work workertype;
+  private Work workertype;
 
-    public Building(int x, int y, int width, int height, int maxResidents, int maxWorkers, int constructionTime, HashMap<Ressource, Integer> constructionCost) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.maxResidents = maxResidents;
-        PlayerInventory.productRessource(Ressource.MAXRESIDENTS, maxResidents);
-        
-        this.currentResidents = 0;
-        this.maxWorkers = maxWorkers;
-        this.currentWorkers = 0;
-        this.constructionTime = constructionTime;
-        this.constructionCost = constructionCost;
+  public Building(int x, int y, int width, int height, int maxResidents,
+                  int maxWorkers, int constructionTime,
+                  HashMap<Ressource, Integer> constructionCost) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.maxResidents = maxResidents;
+    PlayerInventory.productRessource(Ressource.MAXRESIDENTS, maxResidents);
 
-        System.out.println("Building created");
-        build();
+    this.currentResidents = 0;
+    this.maxWorkers = maxWorkers;
+    this.currentWorkers = 0;
+    this.constructionTime = constructionTime;
+    this.constructionCost = constructionCost;
+
+    System.out.println("Building created");
+    build();
+  }
+
+  private boolean isBuildable() {
+    // Check if the building is buildable
+
+    // Check if the player has enough ressources
+    boolean enoughRessources =
+        PlayerInventory.hasEnoughRessources(constructionCost);
+
+    // Check if the building is in a valid position
+    // TODO: Implement this method so that it do not overlap with other
+    // buildings
+    boolean validPosition = true;
+
+    return enoughRessources && validPosition;
+  }
+
+  private void build() {
+    // Check if the building is buildable
+    if (!isBuildable()) {
+      // Throw an exception
+      throw new IllegalArgumentException("The building is not buildable");
     }
 
-    private boolean isBuildable() {
-        // Check if the building is buildable
-
-        // Check if the player has enough ressources
-        boolean enoughRessources = PlayerInventory.hasEnoughRessources(constructionCost);
-
-        // Check if the building is in a valid position
-        // TODO: Implement this method so that it do not overlap with other buildings
-        boolean validPosition = true;
-
-        return enoughRessources && validPosition;
+    // Remove the ressources from the player inventory
+    for (Ressource ressource : constructionCost.keySet()) {
+      PlayerInventory.useRessource(ressource, constructionCost.get(ressource));
     }
 
-    private void build() {
-        // Check if the building is buildable
-        if (!isBuildable()) {
-            // Throw an exception
-            throw new IllegalArgumentException("The building is not buildable");
-        }
+    System.out.println("Building is being built... (waiting " +
+                       constructionTime + " seconds)");
+    new Thread(() -> {
+      try {
+        Thread.sleep(constructionTime * 1000);
+      } catch (InterruptedException e) {
+        System.err.println("Thread was interrupted!");
+      }
+      System.out.println("Building is built!");
 
-        // Remove the ressources from the player inventory
-        for (Ressource ressource : constructionCost.keySet()) {
-            PlayerInventory.useRessource(ressource, constructionCost.get(ressource));
-        }
+      // Set the building as built
+      isBuilt = true;
+    }).start();
+  }
 
-        System.out.println("Building is being built... (waiting " + constructionTime + " seconds)");
-        new Thread(() -> {
-            try {
-                Thread.sleep(constructionTime * 1000);
-            } catch (InterruptedException e) {
-                System.err.println("Thread was interrupted!");
-            }
-            System.out.println("Building is built!");
-
-            // Set the building as built
-            isBuilt = true;
-        }).start();
+  public int addResident(int quantity) {
+    // Check if the building is built
+    if (!isBuilt) {
+      // Throw an exception
+      throw new IllegalArgumentException("The building is not built yet");
     }
 
-    public int addResident(int quantity) {
-        // Check if the building is built
-        if (!isBuilt) {
-            // Throw an exception
-            throw new IllegalArgumentException("The building is not built yet");
-        }
+    // Check if the building has enough space
+    if (currentResidents + quantity > maxResidents) {
+      // Add as many residents as possible and return remaining quantity
+      int remainingResidents = maxResidents - currentResidents;
+      currentResidents += remainingResidents;
 
-        // Check if the building has enough space
-        if (currentResidents + quantity > maxResidents) {
-            // Add as many residents as possible and return remaining quantity
-            int remainingResidents = maxResidents - currentResidents;
-            currentResidents += remainingResidents;
-
-            return quantity - remainingResidents;
-        }
-
-        // Add the residents
-        currentResidents += quantity;
-
-        return 0;
-
+      return quantity - remainingResidents;
     }
 
-    public int addWorkers(int quantity) {
-        // Check if the building is built
-        if (!isBuilt) {
-            // Throw an exception
-            throw new IllegalArgumentException("The building is not built yet");
-        }
+    // Add the residents
+    currentResidents += quantity;
 
-        // Check if the building has enough space
-        if (currentWorkers + quantity > maxWorkers) {
-            // Add as many workers as possible and return remaining quantity
-            int remainingWorkers = maxWorkers - currentWorkers;
-            currentWorkers += remainingWorkers;
+    return 0;
+  }
 
-            return quantity - remainingWorkers;
-        }
-
-        // Add the workers
-        currentWorkers += quantity;
-
-        return 0;
+  public int addWorkers(int quantity) {
+    // Check if the building is built
+    if (!isBuilt) {
+      // Throw an exception
+      throw new IllegalArgumentException("The building is not built yet");
     }
 
-    public Work getWorkerType() {
-        // Return the type of worker that can work in this building
-        return workertype;
+    // Check if the building has enough space
+    if (currentWorkers + quantity > maxWorkers) {
+      // Add as many workers as possible and return remaining quantity
+      int remainingWorkers = maxWorkers - currentWorkers;
+      currentWorkers += remainingWorkers;
+
+      return quantity - remainingWorkers;
     }
 
-    public void printBuilding() {
-        System.out.println("Building at (" + x + ", " + y + ")");
-        System.out.println("Cost: " + constructionCost);
-        System.out.println("Size: " + width + "x" + height);
-        System.out.println("Residents: " + currentResidents + "/" + maxResidents);
-        System.out.println("Workers: " + currentWorkers + "/" + maxWorkers);
+    // Add the workers
+    currentWorkers += quantity;
+
+    return 0;
+  }
+
+  public Work getWorkerType() {
+    // Return the type of worker that can work in this building
+    return workertype;
+  }
+
+  public void consumeResources() {
+    // Check if the building is built
+    if (!isBuilt) {
+      // Throw an exception
+      throw new IllegalArgumentException("The building is not built yet");
     }
+
+    // Check if the building has enough ressources
+    if (!PlayerInventory.hasEnoughRessources(constructionCost)) {
+      // Throw an exception
+      throw new IllegalArgumentException("The building does not have enough "
+                                         + "ressources");
+    }
+
+    // Remove the ressources from the player inventory
+    for (Ressource ressource : constructionCost.keySet()) {
+      PlayerInventory.useRessource(ressource, constructionCost.get(ressource));
+    }
+  }
+
+  public void produceResources() {
+    // Check if the building is built
+    if (!isBuilt) {
+      // Throw an exception
+      throw new IllegalArgumentException("The building is not built yet");
+    }
+
+    int ressourcesProduced = currentWorkers;
+    PlayerInventory.productRessource(Ressource.WOOD, ressourcesProduced);
+    System.out.println(" resources produced by the building at (" + x + ", " +
+                       y + ")");
+  }
+
+  public void printBuilding() {
+    System.out.println("Building at (" + x + ", " + y + ")");
+    System.out.println("Cost: " + constructionCost);
+    System.out.println("Size: " + width + "x" + height);
+    System.out.println("Residents: " + currentResidents + "/" + maxResidents);
+    System.out.println("Workers: " + currentWorkers + "/" + maxWorkers);
+  }
 }
