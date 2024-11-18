d2f2f820b47f079599f2d58b81f8059dc26944fd
Roirtur <arthurmac33@gmail.com>
2024-11-18 11:59:13 +0100

    Fixed all warnings

--- eol/src/main/java/module-info.java
+++ eol/src/main/java/module-info.java
@@ -1,7 +1,16 @@
+
 module eol.jfx {
+
     requires javafx.controls;
+
     requires javafx.fxml;
 
+    requires transitive javafx.graphics;
+
+
+
     opens eol.jfx to javafx.fxml;
+
     exports eol.jfx;
+
 }
