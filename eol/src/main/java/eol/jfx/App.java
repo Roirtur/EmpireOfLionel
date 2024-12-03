3bc6e111127984065598b52c79cb6f88b3355994
Gabriel Ifrim <gabriel.ifrim@etu.u-bordeaux.fr>
2024-12-03 20:45:35 +0100

    Building options (add, remove worker, buildind)

--- eol/src/main/java/eol/jfx/App.java
+++ eol/src/main/java/eol/jfx/App.java
@@ -1,7 +1,9 @@
 package eol.jfx;
 
+import eol.jfx.gamesettings.Difficulty;
+import eol.jfx.managers.GameManager;
+import eol.jfx.ressources.InventoryInitiator;
 import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,18 +15,22 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/eol/jfx/main.fxml"));
-        Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root, 720, 480); // Set initial window size to 720x480
+  @Override
+  public void start(Stage stage) throws IOException {
+    InventoryInitiator.initializeInventory(Difficulty.GODMOD);
+    FXMLLoader fxmlLoader =
+        new FXMLLoader(App.class.getResource("/eol/jfx/main.fxml"));
+    Parent root = fxmlLoader.load();
+    Scene scene =
+        new Scene(root, 720, 480); // Set initial window size to 720x480
 
-        stage.setTitle("Empire Of Lionel");
-        stage.setScene(scene);
-        stage.show();
-    }
+    stage.setTitle("Empire Of Lionel");
+    stage.setScene(scene);
+    stage.show();
+  }
 
-    public static void main(String[] args) {
-        launch();
-    }
+  public static void main(String[] args) {
+    GameManager.startGame();
+    launch();
+  }
 }
--- eol/src/main/java/eol/jfx/managers/GameTime.java
+++ eol/src/main/java/eol/jfx/managers/GameTime.java
@@ -11,89 +11,81 @@ import java.util.TimerTask;
  */
 public class GameTime {
 
-    private static volatile GameTime instance;
-    private final Timer timer;
-    private int seconds;
+  private static volatile GameTime instance;
+  private final Timer timer;
+  private int seconds;
 
-    private final int NIGHT_TIME = 19;
-    private final int DAY_TIME = 7;
-    private boolean night = false;
+  private final int NIGHT_TIME = 19;
+  private final int DAY_TIME = 7;
+  private boolean night = false;
 
-    private long days;
-    private long hours;
+  private long days;
+  private long hours;
 
-    // Private constructor to prevent instantiation
-    private GameTime() {
-        timer = new Timer();
-        seconds = 0;
-    }
+  // Private constructor to prevent instantiation
+  private GameTime() {
+    timer = new Timer();
+    seconds = 0;
+  }
 
-    // Public method to provide access to the single instance
-    public static GameTime getInstance() {
-        GameTime localInstance = instance;
+  // Public method to provide access to the single instance
+  public static GameTime getInstance() {
+    GameTime localInstance = instance;
+    if (localInstance == null) {
+      synchronized (GameTime.class) {
+        localInstance = instance;
         if (localInstance == null) {
-            synchronized (GameTime.class) {
-                localInstance = instance;
-                if (localInstance == null) {
-                    instance = localInstance = new GameTime();
-                }
-            }
+          instance = localInstance = new GameTime();
         }
-        return localInstance;
+      }
     }
+    return localInstance;
+  }
 
-    public void startTimer() {
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                incrementTime();
-            }
-        }, 500, 500);
-    }
+  public void startTimer() {
+    timer.scheduleAtFixedRate(new TimerTask() {
+      @Override
+      public void run() {
+        incrementTime();
+      }
+    }, 500, 500);
+  }
 
-    public void stopTimer() {
-        timer.cancel();
-    }
+  public void stopTimer() { timer.cancel(); }
 
-    private synchronized void incrementTime() {
-        seconds++;
-        GameManager.getInstance().updateTime();
-        checkNight();
-        displayTime();
-    }
+  private synchronized void incrementTime() {
+    seconds++;
+    GameManager.getInstance().updateTime();
+    checkNight();
+    // displayTime();
+  }
 
-    private void displayTime() {
-        System.out.println(formatTime(seconds));
-    }
+  private void displayTime() { System.out.println(formatTime(seconds)); }
 
-    public int getSeconds() {
-        return seconds;
-    }
+  public int getSeconds() { return seconds; }
 
-    private String formatTime(long time) {
-        days = Math.floorDiv(time, 24) + 1;
-        hours = time % 24;
-        return String.format("Day %d, %02d:00", days, hours);
-    }
+  private String formatTime(long time) {
+    days = Math.floorDiv(time, 24) + 1;
+    hours = time % 24;
+    return String.format("Day %d, %02d:00", days, hours);
+  }
 
-    private void checkNight() {
-        if (hours >= NIGHT_TIME || hours < DAY_TIME) {
-            if (!night) {
-                changeNightState();
-            }
-        } else {
-            if (night) {
-                changeNightState();
-            }
-        }
+  private void checkNight() {
+    if (hours >= NIGHT_TIME || hours < DAY_TIME) {
+      if (!night) {
+        changeNightState();
+      }
+    } else {
+      if (night) {
+        changeNightState();
+      }
     }
+  }
 
-    private void changeNightState() {
-        night = !night;
-        GameManager.updateNight();
-    }
+  private void changeNightState() {
+    night = !night;
+    GameManager.updateNight();
+  }
 
-    public static boolean isNight() {
-        return getInstance().night;
-    }
+  public static boolean isNight() { return getInstance().night; }
 }
