47cdad28f396fbda6109dfd63849ef0eb54c52b0
Gabriel Ifrim <gabriel.ifrim@etu.u-bordeaux.fr>
2024-12-04 14:10:54 +0100

    scene manager, load fxmls at start and just change scenes

--- eol/src/main/java/eol/jfx/viewcontrollers/MenuController.java
+++ eol/src/main/java/eol/jfx/viewcontrollers/MenuController.java
@@ -1,13 +1,9 @@
 package eol.jfx.viewcontrollers;
 
+import eol.jfx.gamesettings.SceneManager;
 import eol.jfx.managers.GameManager;
-import java.io.IOException;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
 
 public class MenuController {
 
@@ -15,40 +11,13 @@ public class MenuController {
 
   @FXML
   private void handleStartGame() {
-    try {
-      GameManager.startGame(); // Start the game
-
-      FXMLLoader fxmlLoader =
-          new FXMLLoader(getClass().getResource("/eol/jfx/main.fxml"));
-      Parent root = fxmlLoader.load();
-      Scene scene =
-          new Scene(root, 720, 480); // Set initial window size to 720x480
-
-      // Get the current stage from the startGameButton
-      Stage stage = (Stage)startGameButton.getScene().getWindow();
-      stage.setScene(scene);
-      stage.show();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    GameManager.startGame(); // Start the game
+    SceneManager.getInstance().showScene("main");
   }
 
   @FXML
   private void handleSettings() {
-    try {
-      FXMLLoader fxmlLoader =
-          new FXMLLoader(getClass().getResource("/eol/jfx/settings.fxml"));
-      Parent root = fxmlLoader.load();
-      Scene scene =
-          new Scene(root, 720, 480); // Set initial window size to 720x480
-
-      // Get the current stage from the startGameButton
-      Stage stage = (Stage)startGameButton.getScene().getWindow();
-      stage.setScene(scene);
-      stage.show();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    SceneManager.getInstance().showScene("settings");
   }
 
   @FXML
--- eol/src/main/java/eol/jfx/viewcontrollers/SettingsController.java
+++ eol/src/main/java/eol/jfx/viewcontrollers/SettingsController.java
@@ -1,18 +1,14 @@
 package eol.jfx.viewcontrollers;
 
 import eol.jfx.gamesettings.Difficulty;
+import eol.jfx.gamesettings.SceneManager;
 import eol.jfx.managers.GameManager;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
-import javafx.stage.Stage;
 
 public class SettingsController implements Initializable {
 
@@ -37,19 +33,6 @@ public class SettingsController implements Initializable {
 
   @FXML
   private void handleBack() {
-    try {
-      FXMLLoader fxmlLoader =
-          new FXMLLoader(getClass().getResource("/eol/jfx/menu.fxml"));
-      Parent root = fxmlLoader.load();
-      Scene scene =
-          new Scene(root, 720, 480); // Set initial window size to 720x480
-
-      // Get the current stage from the difficultyComboBox
-      Stage stage = (Stage)difficultyComboBox.getScene().getWindow();
-      stage.setScene(scene);
-      stage.show();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    SceneManager.getInstance().showScene("menu");
   }
 }
