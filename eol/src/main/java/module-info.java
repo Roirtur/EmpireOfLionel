031a110a09e51d3388b9d6a400e20685e0152637
Roirtur <arthurmac33@gmail.com>
2024-12-04 13:38:26 +0100

    module info

--- eol/src/main/java/module-info.java
+++ eol/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module eol.jfx {
   requires javafx.controls;
   requires javafx.fxml;
   requires java.base;
-
+  requires transitive javafx.graphics;
   opens eol.jfx.viewcontrollers to javafx.fxml;
 
   exports eol.jfx;
